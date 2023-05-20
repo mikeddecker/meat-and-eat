76f1b03f3ef416c9411f790826b6a8803017aeb2
mikeddecker <mike.dedecker@student.hogent.be>
2023-05-20 13:27:58 +0200

    Adding reactive keyboard

--- src/components/AddMeetupLocation.js
+++ src/components/AddMeetupLocation.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import React, { useRef } from 'react'
 import globalStyles from '../global/globalStyles'
 import { TextInput } from 'react-native-paper'
 import { Formik } from 'formik';
@@ -7,87 +7,95 @@ import { MaterialIcons } from '@expo/vector-icons';
 import * as yup from 'yup';
 
 const AddMeetupLocation = ({ onClose, addLocation }) => {
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
+  const touchableRef = useRef(null);
+
   return (
-    <View style={[globalStyles.container, styles.indent]}>
-      <MaterialIcons name="close" size={48} color="black" style={styles.icon} onPress={onClose} />
-      <Formik
-        initialValues={{ title: '', address: '', description: '', stars: '1' }}
-        onSubmit={location => {
-          addLocation(location)
-          onClose()
-        }}
-        style={styles.formikContainer}
-        validationSchema={yup.object().shape({
-          title: yup
-            .string()
-            .min(4)
-            .required('Please, provide a title!'),
-          address: yup
-            .string()
-            .min(4)
-            .required('Please, provide an address!'),
-          description: yup
-            .string()
-            .min(8)
-            .required('Please, provide a description!'),
-          stars: yup
-            .number()
-            .min(1)
-            .max(5)
-            .required('Please, provide a number of stars!'),
-        })}
-      >
-        {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
-          <View style={styles.form}>
-            <Text style={styles.label}>Title</Text>
-            <TextInput
-              style={styles.textInput}
-              onChangeText={handleChange('title')}
-              onBlur={handleBlur('title')}
-              value={values.title}
-            />
-            <Text style={styles.error}>{touched.title && errors.title}</Text>
-
-            <Text style={styles.label}>Addres</Text>
-            <TextInput
-              style={styles.textInput}
-              onChangeText={handleChange('address')}
-              onBlur={handleBlur('address')}
-              value={values.address}
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <View style={[globalStyles.container, styles.indent]} ref={touchableRef}>
+        <MaterialIcons name="close" size={48} color="black" style={styles.icon} onPress={onClose} />
+        <Formik
+          initialValues={{ title: '', address: '', description: '', stars: '1' }}
+          onSubmit={location => {
+            addLocation(location)
+            onClose()
+          }}
+          style={styles.formikContainer}
+          validationSchema={yup.object().shape({
+            title: yup
+              .string()
+              .min(4)
+              .required('Please, provide a title!'),
+            address: yup
+              .string()
+              .min(4)
+              .required('Please, provide an address!'),
+            description: yup
+              .string()
+              .min(8)
+              .required('Please, provide a description!'),
+            stars: yup
+              .number()
+              .min(1)
+              .max(5)
+              .required('Please, provide a number of stars!'),
+          })}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+            <View style={styles.form}>
+              <Text style={styles.label}>Title</Text>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={handleChange('title')}
+                onBlur={handleBlur('title')}
+                value={values.title}
               />
-            <Text style={styles.error}>{touched.address && errors.address}</Text>
+              <Text style={styles.error}>{touched.title && errors.title}</Text>
 
-            <Text style={styles.label}>Description</Text>
-            <TextInput
-              style={styles.textInput}
-              onChangeText={handleChange('description')}
-              onBlur={handleBlur('description')}
-              value={values.description}
-              multiline={true}
-              numberOfLines={3}
-            />
-            <Text style={styles.error}>{touched.description && errors.description}</Text>
+              <Text style={styles.label}>Addres</Text>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={handleChange('address')}
+                onBlur={handleBlur('address')}
+                value={values.address}
+                />
+              <Text style={styles.error}>{touched.address && errors.address}</Text>
 
-            <Text style={styles.label}>Stars (1-5)</Text>
-            <TextInput
-              style={styles.textInput}
-              onChangeText={handleChange('stars')}
-              onBlur={handleBlur('stars')}
-              value={values.stars}
-              inputMode='numeric'
-            />
-            <Text style={styles.error}>{touched.stars && errors.stars}</Text>
-            
-            <TouchableOpacity 
-              disabled={Object.keys(errors).length > 0} 
-              style={[styles.button, Object.keys(errors).length > 0 ? styles.backgroundColorGreyBlue : styles.backgroundColorBlue]} 
-              onPress={handleSubmit}>
-              <Text style={styles.buttonText}>Add new meat up</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      </Formik>
-    </View>
+              <Text style={styles.label}>Description</Text>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={handleChange('description')}
+                onBlur={handleBlur('description')}
+                value={values.description}
+                multiline={true}
+                numberOfLines={3}
+              />
+              <Text style={styles.error}>{touched.description && errors.description}</Text>
+
+              <Text style={styles.label}>Stars (1-5)</Text>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={handleChange('stars')}
+                onBlur={handleBlur('stars')}
+                value={values.stars}
+                inputMode='numeric'
+              />
+              <Text style={styles.error}>{touched.stars && errors.stars}</Text>
+              
+              <TouchableOpacity 
+                disabled={Object.keys(errors).length > 0} 
+                style={[styles.button, Object.keys(errors).length > 0 ? styles.backgroundColorGreyBlue : styles.backgroundColorBlue]} 
+                onPress={handleSubmit}>
+                <Text style={styles.buttonText}>Add new meat up</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </Formik>
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
@@ -97,20 +105,15 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 24,
     marginTop: 8,
-    marginBottom: 2,
+    marginBottom: 12,
     color: 'black'
   },
-  // textInput: {
-  //   width: '100%',
-  //   borderRadius: 53,
-  // },
   textInput: {
     width: '100%',
     borderTopLeftRadius: 10, // TextInput gets borderTopLeftRadius: 4 if nothing specified
     borderTopRightRadius: 10,
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
-    // borderBottomWidth: 0,
   },
   form: {
     width: '100%',
